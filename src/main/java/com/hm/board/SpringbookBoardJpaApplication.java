7edfab5cfdcbef5ecb7c16e2aaf502ef80686b73
junglestory <monocaine@gmail.com>
2023-05-06 10:42:05 +0900

    First commit

---
+++ src/main/java/com/hm/board/SpringbookBoardJpaApplication.java
@@ -0,0 +1,13 @@
+package com.hm.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbookBoardJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbookBoardJpaApplication.class, args);
+	}
+
+}
